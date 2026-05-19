17453f1309e6070840686e961213d8500ab0e85a
John DeLuce <johnrdeluce@gmail.com>
2026-05-19 13:52:26 -0400

    Add output for instance type and count

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "public_dns_name" {
   description = "Public DNS names of the load balancer for this project"
   value       = module.elb_http.this_elb_dns_name
 }
+
+output "instance_type" {
+  description = "EC2 instance type"
+  value       = var.instance_type
+}
+
+output "instance_count" {
+  description = "Amount of EC2 instances"
+  value       = var.instance_count
+}
